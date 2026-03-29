cc1cac54e7b829a08a32ecfe70ef14783089f7d7
Harsh Arya <harsharya8946@gmail.com>
2026-03-29 08:15:53 +0530

    back button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let chngbtn = document.querySelector('#chngtheme');
 let main = document.querySelector('main');
 let allElem = document.querySelectorAll('.elem');
 let afelem = document.querySelectorAll('.full-elem');
+let backbtn = document.querySelectorAll('.back');
 
 //change theme
 let btn = true;
@@ -17,10 +18,16 @@ chngbtn.addEventListener('click', function(){
     }
 })
 
-
+//elements to full screen
 allElem.forEach(function(elem){
     elem.addEventListener('click',function(){
-        console.log(afelem[elem.id]);
         afelem[elem.id].style.display = 'block';
+    })
+})
+
+//back button
+backbtn.forEach(function(elem){
+    elem.addEventListener('click', function(){
+        afelem[elem.id].style.display = 'none';
     })
 })
