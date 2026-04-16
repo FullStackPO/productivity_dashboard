ddf81e27c3b089906073c03c9011783ed4050fa3
Harsh Arya <harsharya8946@gmail.com>
2026-04-16 23:07:38 +0530

    adding clock

--- script.js
+++ script.js
@@ -39,6 +39,26 @@ backbtn.forEach(function(elem){
     })
 })
 
+//clock
+function updateClock() {
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  const time = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").textContent = time;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
+
 //motivation quotes
 function getLine(){
     let ran = Math.floor(Math.random()*motivation.length);
